e31da77f567584cd121343551338705132f967b9
Rafael Martinez Montiel <rafael.martinez.montiel@gmail.com>
2023-07-19 12:00:14 -0600

    Allow storage of athlete credentials and application credentials

--- credentials/credentials/store_credentials.py
+++ credentials/credentials/store_credentials.py
@@ -4,8 +4,12 @@ def write_credentials(credentials, table):
     """
     Write credentials to storage
     """
-    check_keys_in_dict(credentials, ['id', 'access_token', 'refresh_token','credential_type', 'expires_at'])
-
+    check_keys_in_dict(credentials, ['id','credential_type'])
+    check_value_in_set(credentials['credential_type'], ['athlete','application'])
+    if credentials['credential_type'] == 'athlete':        
+        check_keys_in_dict(credentials, ['access_token', 'refresh_token', 'expires_at'])
+    elif credentials['credential_type'] == 'application':
+        check_keys_in_dict(credentials, ['application_id', 'application_secret'])
     item = create_item(credentials)
     table.put_item(Item=item)
     return {
@@ -22,11 +26,20 @@ def create_item(credentials):
         item[key] = credentials[key]
     return item
 
+
+def check_value_in_set(value, set):
+    for valid_value in set:
+        if value == valid_value:
+            return
+    raise ValueError(f'Value not valid: {value}')
+
+    
+
 def check_keys_in_dict(dict, keys):
     """
     Check if all keys in keys are in dict
     """
     for key in keys:
         if not key in dict:
-            raise ValueError('Missing key: ' + key)
+            raise ValueError(f'Missing key: {key}')
     pass
--- credentials/tests/unit/test_store_credentials.py
+++ credentials/tests/unit/test_store_credentials.py
@@ -5,17 +5,38 @@ from unittest.mock import Mock
 
 from credentials import store_credentials
 from unittest import result
+import sys
 
-
+sys.path.append("../../credentials")
 @pytest.fixture()
 def store_credentials_event():
     return { 
             "id":"sample-id",
             "access_token":"sample-access-token",
             "refresh_token":"sample-refresh-token",
-            "credential_type":"sample-credentials-type",
+            "credential_type":"athlete",
             "expires_at":"2024-01-01"
         }
+
+@pytest.fixture()
+def store_app_credentials_event():
+    return { 
+            "id":"sample-id",
+            "application_id":"sample-application-id",
+            "application_secret":"sample-application-secret",
+            "credential_type":"application"
+        }
+
+
+@pytest.fixture()
+def store_app_credentials_event_invalid_type():
+    return { 
+            "id":"sample-id",
+            "application_id":"sample-application-id",
+            "application_secret":"sample-application-secret",
+            "credential_type":"invalid-credential-type"
+        }
+
 @pytest.fixture()
 def mocker():
     return Mock()
@@ -26,8 +47,15 @@ def test_store_credentials(store_credentials_event, mocker):
         assert result['id'] == store_credentials_event['id']
         assert result['status'] == 'success'
     except Exception as e:
-        pytest.fail(f"Unexpected exception: {e}")
+        pytest.fail(f"Unexpected exception: {str(e)}",e)
 
+def test_store_app_credentials(store_app_credentials_event, mocker):
+    try:
+        result = store_credentials.write_credentials(store_app_credentials_event, mocker)
+        assert result['id'] == store_app_credentials_event['id']
+        assert result['status'] == 'success'
+    except Exception as e:
+        pytest.fail(f"Unexpected exception: {str(e)}",e)
 
 def test_store_credentials_invalid_event(mocker):
     with pytest.raises(Exception):
@@ -40,3 +68,7 @@ def test_store_credentials_ddb_exception(store_credentials_event, mocker):
     mocker.put_item.side_effect = Exception()
     with pytest.raises(Exception):
         store_credentials.write_credentials(store_credentials_event, mocker)
+
+def test_store_credentials_invalid_event_type(store_app_credentials_event_invalid_type,mocker):
+    with pytest.raises(Exception):
+        store_credentials.write_credentials(store_app_credentials_event_invalid_type, mocker)
